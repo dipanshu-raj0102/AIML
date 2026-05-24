8e5a096d901f14093c4c91af73c5c965ce418e9c
Dipanshu Raj <dipanshuraj0102@gmail.com>
2026-05-24 17:32:29 +0530

    pytorch

---
+++ tensor_flow/nural1.py
@@ -0,0 +1,13 @@
+import tensorflow as tf 
+import numpy as np 
+
+model = tf.keras.Sequential([tf.keras.Input(shape = (1,)),tf.keras.layers.Dense(units=1)])
+
+model.compile(optimizer = 'sgd', loss = 'mean_squared_error')
+
+X = np.array([-1.0,  0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+Y= np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
+
+model.fit(X, Y, epochs = 500)
+
+print(model.predict(np.array([10])))
