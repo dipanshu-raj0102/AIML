a130ed551f425df1b4856ea9985d80c0e9ec06ee
Dipanshu Raj <dipanshuraj0102@gmail.com>
2026-07-29 21:30:32 +0530

    added comments to gridworld

--- AI_LAB/gridworld.py
+++ AI_LAB/gridworld.py
@@ -14,16 +14,19 @@ def reach_goal(grid, source, goal):
         "vertical": True,
     }
 
+    # if distance is negative reverse direction and set flag to False for row
     if (horizdist < 0):
         flag["horizontal"] = False
         direction["horizontal"] = "left"
         horizdist = -horizdist
 
+    # if distance is negative reverse vertical movement direction and set flag to false
     if (vertdist < 0):
         flag["vertical"] = False
         direction["vertical"] = "up"
         vertdist = -vertdist
 
+    # till horizontal distance becomes zero, move to respective direction horizontally step by step
     while(horizdist > 0):
         if(flag["horizontal"]):
             source["row"] += 1
@@ -33,6 +36,7 @@ def reach_goal(grid, source, goal):
         print(f"Move {direction["horizontal"]}: Position({source["row"], source["col"]})")
         horizdist -= 1
     
+    # till vertical distance becomes zero, move to respective direction vertically step by step
     while(vertdist > 0):
         if(flag["vertical"]):
             source["col"] += 1
@@ -45,11 +49,14 @@ def reach_goal(grid, source, goal):
     return 0
 
 
+# function to get dimention. use"dimension of grid" for grid and position of source/goal for their position resp.
 def get_dim(name):
     row, col = input(f"Enter {name} (row col): ").split(" ")
     row = int(row)
     col = int(col)
+
     return row, col
+
 
 def main():
     grid = {
@@ -70,19 +77,24 @@ def main():
     grid["row"], grid["col"] = get_dim("dimension of Grid")
 
     source["row"], source["col"] = get_dim("position of source")
-    
+
+    #checks whether the source is under the boundary of grid
     if ((source["row"] < 0 or source["row"] > grid["row"]) or (source["col"] < 0 or source["col"] > grid["col"])):
         print("Error!! Position of source cannot be less than 0 or greater than grid size")
         sys.exit(1)
 
     goal["row"], goal["col"] = get_dim("position of goal")
     
+    # checks whether the goal is under the boundary of grid
     if ((goal["row"] < 0 or goal["row"] > grid["row"]) or (goal["col"] < 0 or goal["col"] > grid["col"])):
         print("Error!! Position of goal cannot be less than 0 or greater than grid size")
         sys.exit(1)
 
+
+    # calls function reach_goal() to perform agent movement
     status = reach_goal(grid, source, goal)
 
+    # checks the return status of reach_goal() function. 0 == success
     if (status == 0):
         print(f"Reached Goal({goal["row"], goal["col"]})")
         print("Agent Shutting Down!! GoodBye!")
