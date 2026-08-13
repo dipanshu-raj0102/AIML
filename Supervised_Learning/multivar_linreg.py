609ad21bfccae9389f85386a43d3960a303e38bb
Dipanshu Raj <dipanshuraj0102@gmail.com>
2026-08-13 19:12:36 +0530

    supervised learning

---
+++ Supervised_Learning/multivar_linreg.py
@@ -0,0 +1,9 @@
+import math, copy 
+import numpy as np 
+import matplotlib.pyplot as plt 
+np.set_printoptions(precision = 2)
+
+X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
+y_train = np.array([460, 232, 178])
+
+
